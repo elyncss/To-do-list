01b961e9fac3bf99044ecdbc2025f55cdb034df9
elyncss <elkhanmn@code.edu.az>
2022-11-29 18:43:28 +0400

    new updates

--- todo.js
+++ todo.js
@@ -3,10 +3,12 @@ const task = document.querySelector("#to-do-input");
 const listBox = document.querySelector("ul");
 const taskBox = document.querySelector(".tasks");
 const xbtn = document.querySelector(".input-icon");
+const sort = document.querySelector(".icon-arrow");
 
 let taskArr = [];
 
-addTask.addEventListener("click", () => {
+addTask.addEventListener("click", (e) => {
+  e.preventDefault();
   const newList = document.createElement("li");
 
   const newImg = document.createElement("img");
@@ -15,14 +17,15 @@ addTask.addEventListener("click", () => {
   taskBox.style.display = "block";
   taskArr.push(task.value);
 
-  for (let i = 0; i < taskArr.length; i++) {
-    newList.textContent = taskArr[i];
-  }
-
   listBox.append(newList);
   newList.append(newImg);
+
   localStorage.setItem("elxan", JSON.stringify(taskArr));
   let data = JSON.parse(localStorage.getItem("elxan"));
+
+  data.array.forEach((element) => {
+    newList.innerHTML = element;
+  });
 
   newImg.addEventListener("click", () => {
     if (taskArr.length < 2) {
@@ -40,6 +43,29 @@ addTask.addEventListener("click", () => {
 
 xbtn.addEventListener("click", () => {
   task.value = "";
+});
+
+// sort icon **********************************************
+
+sort.addEventListener("mouseover", () => {
+  sort.src = "icons/down2.svg";
+});
+
+sort.addEventListener("mouseleave", () => {
+  sort.src = "icons/down1.svg";
+});
+
+sort.addEventListener("click", () => {
+  taskArr.sort();
+  sort.src = "icons/up2.svg";
+
+  sort.addEventListener("mouseleave", () => {
+    sort.src = "icons/up1.svg";
+  });
+
+  sort.addEventListener("mouseover", () => {
+    sort.src = "icons/up2.svg";
+  });
 });
 
 //=========================================
